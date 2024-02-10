cec9cc368cc165d7600e71b92ee9287fa056b65b
l0st-idi0t <carlnod121@gmail.com>
2024-02-10 13:18:34 -0600

    console log the port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require("express")
-const app = express()
-const path = require("path")
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
 
-app.get('/', function (req, res) {
-  res.redirect("/homepage.html")
-})
+app.get("/", function (req, res) {
+  res.redirect("/homepage.html");
+});
 
-app.use(express.static(path.resolve(__dirname, "public")))
+app.use(express.static(path.resolve(__dirname, "public")));
 
-app.listen(3000);
+console.log(`Server is running on http://localhost:${port}/`);
+app.listen(port);
