230ee4f2a40ca469f14f0dff20c3624ea0788840
JLian21UIC <jlian21@uic.edu>
2024-02-10 14:38:30 -0600

    note is now a button element

--- public/board.js
+++ public/board.js
@@ -1,11 +1,21 @@
 const notesContainer = document.getElementById("todayNotesContent");
 
-function createNote(id, text) {
-  const note = document.createElement("textarea");
-  note.setAttribute("readonly", "true");
+// function createNote(id, text) {
+//   const note = document.createElement("textarea");
+//   note.setAttribute("readonly", "true");
 
+//   note.classList.add("note");
+//   note.value = text;
+
+//   notesContainer.appendChild(note);
+// }
+
+function createNote(id, text) {
+  const note = document.createElement("button");
+ 
+  note.setAttribute("onclick", "placeholder()")
+  note.innerHTML = text;
   note.classList.add("note");
-  note.value = text;
 
   notesContainer.appendChild(note);
 }
