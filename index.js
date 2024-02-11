01783a0060fe2355c7dd439a412b60a5d3550613
Sublimeful <Qiangwu2000@gmail.com>
2024-02-11 05:46:12 +0000

    Switch to reverse proxy approach

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 import express from "express";
 import path from "path";
-import fs from "fs";
-import https from "https";
 import dotenv from "dotenv";
 
 dotenv.config({});
 
 const app = express();
 const port = process.env.PORT;
-const privateKey = fs.readFileSync('./certs/privkey.pem');
-const certificate = fs.readFileSync('./certs/cert.pem');
 
 app.use(express.static(path.resolve("public")));
-
-app.use(function(request, response, next) {
-  if (process.env.NODE_ENV != 'development' && !request.secure) {
-    return response.redirect("https://" + request.headers.host + request.url);
-  }
-  next();
-})
 
 app.get("/", function (req, res) {
   res.redirect("/homepage.html");
@@ -26,7 +15,4 @@ app.get("/", function (req, res) {
 
 console.log(`Server is running on port ${port}`);
 
-https.createServer({
-  key: privateKey,
-  cert: certificate
-}, app).listen(port);
+app.listen(port);
